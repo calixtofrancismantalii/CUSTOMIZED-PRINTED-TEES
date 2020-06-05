b2429e9934bcf41a398d99da7b56c839f5319fae
Cyries <cyriesmadrid@gmail.com>
2020-06-05 17:14:00 +0800

    Home page Desktop view and Ipad view fix

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function openModal(){
+    var modal;
+    modal = document.getElementById("modal");
+    modal.style.display = "block";
+}
+function closeModal(){
+    var modal;
+    modal = document.getElementById("modal");
+    modal.style.display = "none";
+}
