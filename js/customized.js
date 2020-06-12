3550d110d7feeea7447094fe58a1611489e45d64
Cyries <cyriesmadrid@gmail.com>
2020-06-12 15:24:30 +0800

    connected pages form home to personalized page

---
+++ js/customized.js
@@ -0,0 +1,13 @@
+function openModal(){
+    var modal;
+    modal = document.getElementById("modal");
+    
+    modal.style.display = "block";
+}
+
+function closeModal(){
+    var modal;
+    modal = document.getElementById("modal");
+    
+    modal.style.display = "none";
+}
