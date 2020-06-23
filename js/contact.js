cc98f8562419cb7f433c78fe3fb14aa066da2939
Cyries <cyriesmadrid@gmail.com>
2020-06-23 14:31:51 +0800

    remove uncessary script

--- js/contact.js
+++ js/contact.js
@@ -1,15 +1,3 @@
-//function initMap(){
-//    var location, map;
-//    location = {lat: 14.599512, lang: 120.984222};
-//    map = new google.maps.Map(document.getElementById("map"), {
-//        zoom: 4,
-//        center: location
-//    });
-//    
-//}
-
-
-
 function myMap() {
 var mapProp = {
     center:new google.maps.LatLng(14.599512,120.984222),
